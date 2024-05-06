362f6305278ad614f656da6ed0436730e71299e0
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-05-06 17:28:48 -0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
   // set exam date to be may 7 at 7:30 am mst
   const examDate: any = new Date("2024-05-07T07:30:00-07:00");
-  const [timeTill, setTimeTill] = useState(["0", "0", "0", "0"]);
+  const [timeTill, setTimeTill] = useState(["00", "00", "00", "00"]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -36,7 +36,7 @@ export default function Home() {
 
   return (
     <main className="bg-black flex min-h-screen flex-col items-center justify-center sm:p-24 p-4">
-      <p className="text-lg text-white/40 mb-2">Time until exam:</p>
+      <p className="text-lg text-white/40 mb-4">Time until exam:</p>
       <p className="text-3xl sm:text-6xl text-white font-mono font-normal">
         {timeTill[0]}:{timeTill[1]}:{timeTill[2]}:{timeTill[3]}
       </p>
