96afb0ec5c4645a5b401ebea423a7101d9a7aa80
dickeyy <kyledickey02@gmail.com>
2024-05-06 20:51:00 -0600

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Countdown from "@/components/countdown";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-24">
+        <main className="flex h-screen flex-col items-center justify-center p-4">
             <Countdown />
         </main>
     );
